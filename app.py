1c68cc33e89fc4f23eeac38a6425fdebb35d8676
Sergio <sergioalfrancaj@gmail.com>
2026-05-10 19:50:43 +0200

    postgresql

--- app.py
+++ app.py
@@ -39,6 +39,7 @@ if uri and uri.startswith("postgres://"):
     uri = uri.replace("postgres://", "postgresql://", 1)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = uri
+print("🔥 DATABASE URL REAL:", uri)
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db.init_app(app)
